ca03008db612f626d3d7a30aedd79e1804c0f464
ErikBorgogno <erikborgo.20@gmail.com>
2022-10-13 23:18:06 +0200

    change view show for tags

--- resources/views/admin/tags/show.blade.php
+++ resources/views/admin/tags/show.blade.php
@@ -22,32 +22,30 @@
             <ul class="list-unstyled pt-5 d-flex justify-content-center  flex-column">
                 <li class="text-secondary py-1"><h4 class="text-white">Title: {{$tag->name}}</h4></li>
             </ul>
-            @if(count($tag->posts))
-                <table class="table m-auto table-dark pb-5">
-                    <thead class="">
-                        <tr class="text-primary">
-                            <th scope="col">#</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Slug</th>
-                            <th scope="col">Details</th>
+            <table class="table m-auto table-dark pb-5">
+                <thead class="">
+                    <tr class="text-primary">
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($tag->posts as $post)
+                        <tr>
+                            <th scope="row">{{$post->id}}</th>
+                            <td>{{$post->title}}</td>
+                            <td>{{$post->slug}}</td>
+                            <td><a class="btn btn-primary" href="{{route('admin.posts.show', ['post' => $post])}}">Details</a></td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($tag->posts as $post)
-                            <tr>
-                                <th scope="row">{{$post->id}}</th>
-                                <td>{{$post->title}}</td>
-                                <td>{{$post->slug}}</td>
-                                <td><a class="btn btn-primary" href="{{route('admin.posts.show', ['post' => $post])}}">Details</a></td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <th>Not result found!</th>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-            @endif
+                    @empty
+                        <div>
+                            <h4 class="text-warning">Not result found!</h4>
+                        </div>
+                    @endforelse
+                </tbody>
+            </table>
             <a href="{{route('admin.tags.index')}}" class="btn btn-primary my-1">Back</a>
         </div>
     </div>
